f97ca99fb940b9eef546bd35bb6c8781a20b2fcc
Gerardo Sanchez <oracle942@gmail.com>
2021-09-13 16:57:47 -0700

    Added template literals and writeFile function

--- index.js
+++ index.js
@@ -13,12 +13,26 @@ const dom = new JSDOM(`<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
-    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Team Profile Generator</title>
+
 </head>
 <body>
-    <p>Hello World</p>
-    <script src="../script.js"></script>
+
+    <div class="container">
+        <header class="d-flex justify-content-center py-3">
+          <h1>TEAM PROFILE GENERATOR</h1>
+        </header>
+    </div>
+
+    <div class = "content"></div>
+      
+    
+    <script src="../script.js">
+    </script>
+   
 </body>
 </html>`);
    
@@ -34,36 +48,24 @@ const questions = [
     {
     type: 'input',
     message: "Please enter the team manager's name:",
-    name: 'manager-name'  
+    name: 'managerName'  
 },
     {
     type: 'input',
-    message: "Please enter a phone number:",
-    name: 'phone-number'  
-}
-// {
+    message: "Please enter the team manager's phone number:",
+    name: 'phoneNumber'  
+},
+    {
+    type: 'input',
+    message: "Please enter the team manager's email:",
+    name: 'email'  
+},
+    {
+    type: 'input',
+    message: "Please enter the team manager's office number:",
+    name: 'officeNumber'  
+},
 
-//     type: 'list',
-//     name: 'list',
-//     message: "Please select an employee to ad, or finish",
-//     choices: ["engineer", "intern", "finish"],
-    // choices => if(choices === "finish") break
-    
-    // }
-//     {
-//     type: 'input',
-//     message: "Please enter the engineer's github url:",
-//     name: 'github-url',
-//     when: (answers) => answers.list === 'engineer'
-
-// },
-//     {
-//     type: 'input',
-//     message: "Please enter the intern's school:",
-//     name: 'school',
-//     when: (answers) => answers.list === 'intern'
-
-// }
 
 ]
 
@@ -117,17 +119,23 @@ function init() {
     
     { function getAnswers(){
         var div = dom.window.document.createElement("div")
-        var body = dom.window.document.querySelector("body")
-        // var h1 = dom.window.document.createElement("h1")
-        div.innerHTML = `lkajsdfldsjkf`
-        // const card = (answers) => `${answers.manager-name}`
-        body.appendChild(div)
-        var domString = dom.window.document.documentElement.outerHTML
-        // console.log(dom.textContent)
-        // console.log(dom.window.document.querySelector("body").textContent); // "Hello world"
-        fs.writeFile('index1.html', domString, (err) =>
-            err ? console.log(err) : console.log('Successfully created index.html!')
-          );
+        var content = dom.window.document.querySelector(".content")
+        div.innerHTML = `<div class="container d-flex flex-wrap">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+            <h5 class="card-title">${answers.managerName}</h5>
+            <p class="card-text">Manager</p>
+
+            </div>
+            <ul class="list-group list-group-flush">
+            <li class="list-group-item">Phone number: ${answers.phoneNumber}</li>
+            <li class="list-group-item">Email: ${answers.email}</li>
+            <li class="list-group-item">Office Number: ${answers.officeNumber}</li>
+            </ul>
+            
+        </div>    
+    </div>`
+        content.appendChild(div)
 
 
        
@@ -137,14 +145,46 @@ function init() {
 
 
             if(answers2.list === "engineer") { inquirer.prompt(questions3).then(
-                (answers3) =>  {manager = new Manager(answers3);
-                               manager.getName(answers3.name);
-                               manager.getId(answers3.id);
-                               manager.getEmail(answers3.email);
-                               manager.getRole("Engineer");
-                               console.log(manager.email)}).then(
+                (answers3) =>  {engineer = new Engineer(answers3);
+                               name = engineer.getName(answers3.name);
+                               id = engineer.getId(answers3.id);
+                               email = engineer.getEmail(answers3.email);
+                               role = engineer.getRole("Engineer");
+                               github = engineer.getGitHub(answers3.github)
+                               div.innerHTML = `<div class="container d-flex flex-wrap">
+                               <div class="card" style="width: 18rem;">
+                                   <div class="card-body">
+                                   <h5 class="card-title">${answers3.name}</h5>
+                                   <p class="card-text">Engineer</p>
+                       
+                                   </div>
+                                   <ul class="list-group list-group-flush">
+                                   <li class="list-group-item">Employee ID: ${answers3.id}</li>
+                                   <li class="list-group-item">Email: ${answers3.email}</li>
+                                   <li class="list-group-item">GitHub: ${answers3.github}</li>
+                                   </ul>
+                                   
+                               </div>    
+                           </div>`
+                           var div2 = dom.window.document.createElement("div")
+                           var domString = dom.window.document.documentElement.outerHTML;
+                           content.appendChild(div2)
+                            fs.writeFile('index1.html', domString, (err) =>
+                                err ? console.log(err) : console.log('Successfully created index.html!')
+                            );
+                            
+                            
+                            
+                            }
 
-                (answers2) => getAnswers())
+
+                               
+                               
+                               
+                               
+                               ).then(
+
+                (answers3) => getAnswers())
             }
 
             else if(answers2.list === "intern") {inquirer.prompt(questions4).then((answers4) => 
